9a4c5f9721d695a8e29f8ed85f53fe1aca494c37
Olexandr Nesteruk <54321777tv@gmail.com>
2023-11-21 11:59:18 +0200

    User and registration features

--- src/main/java/com/bookstore/dto/book/BookSearchParameters.java
+++ src/main/java/com/bookstore/dto/book/BookSearchParameters.java
@@ -1,4 +1,4 @@
-package com.bookstore.dto;
+package com.bookstore.dto.book;
 
 public record BookSearchParameters(String[] title, String[] author, String[] isbn, String[] price) {
 
---
+++ src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.bookstore.service.impl;
+
+import com.bookstore.dto.user.UserRegistrationRequestDto;
+import com.bookstore.dto.user.UserResponseDto;
+import com.bookstore.exception.RegistrationException;
+import com.bookstore.mappers.UserMapper;
+import com.bookstore.model.User;
+import com.bookstore.repository.user.UserRepository;
+import com.bookstore.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto request)
+            throws RegistrationException {
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RegistrationException("User with this email already exist");
+        }
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setShippingAddress(request.getShippingAddress());
+        User savedUser = userRepository.save(user);
+        return userMapper.mapToDto(savedUser);
+    }
+}
